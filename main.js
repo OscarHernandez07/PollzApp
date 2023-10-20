e5e3131c1ed949055c1772a65bd3279150a1cb19
Oscar Hernandez <129766607+OscarHernandez07@users.noreply.github.com>
2023-10-20 12:44:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const poll = document.querySelector('.poll')
+const buttons = poll.querySelectorAll('button')
+
+buttons[0].addEventListener('click', vote);
+function vote(e) {
+    console.log(e.target.innerHTML);
+} 
+
+buttons[1].addEventListener('click', vote);
+
+buttons[2].addEventListener('click', vote);
+
+buttons[3].addEventListener('click', vote);
